acda84b3a074388f404b7639b8132b2418ec469d
elleryq <elleryq@gmail.com>
2021-06-23 09:44:47 +0800

    Update server.js
    Add version

--- app/server.js
+++ app/server.js
@@ -4,10 +4,11 @@ const express = require('express');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const VERSION = "1.0.0";
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send(`Hello world! ${VERSION}\n`);
 });
 
 app.listen(PORT, HOST);
